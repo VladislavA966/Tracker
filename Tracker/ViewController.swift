33bddf8ba70c182dba7ca2fdb07c99ff0ac05aa1
v_afonin <Vladislav.Afonin@mbank.kg>
2026-07-30 10:30:23 +0600

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Владилсав on 30/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
